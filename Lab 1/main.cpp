384131bb65e0e550c2a895de8ef71434b14b8266
Anatoly Samaris <92950839+AnatolySamaris@users.noreply.github.com>
2022-09-16 10:00:30 +0300

    Test of the class

--- Lab 1/main.cpp
+++ Lab 1/main.cpp
@@ -1,70 +1,27 @@
 #include "ChessTable.h"
-#include <string>
-#include <vector>
 
 using namespace std;
-
-vector<string> split(string &s)
-{
-    vector<string> ans;
-    string word;
-    for (auto i: s + " ")
-    {
-        if (i == ' ')
-        {
-            ans.push_back(word);
-            word = "";
-        }
-        else word.push_back(i);
-    }
-    return ans;
-}
-
-int to_int(char num)
-{
-    return (int)num - '0';
-}
 
 int main()
 {
     ChessTable chess;
-    bool game = true;
-    string act;
-    while (game)
-    {
-        cout << "Enter your command: ";
-        cin >> act;
-        vector<string> to_do = split(act);
 
-        if (to_do[0] == "MOVE")
-        {
-            chess.move(to_do[1][0], to_int(to_do[2][0]), to_do[3][0], to_int(to_do[4][0]));
-        }
-        else if (to_do[0] == "ADD")
-        {
-            chess.add_figure(to_do[1][0], to_do[2][0], to_int(to_do[3][0]));
-        }
-        else if (to_do[0] == "AUTO")
-        {
-            chess.autofill();
-        }
-        else if (to_do[0] == "CLEAR")
-        {
-            chess.clear();
-        }
-        else if (to_do[0] == "SHOW")
-        {
-            chess.show();
-        }
-        else if (to_do[0] == "HELP")
-        {
-            chess.help();
-        }
-        else if (to_do[0] == "END")
-        {
-            game = false;
-        }
-    }
+    chess.autofill();
+    chess.show();
 
-    cout << "Game over!" << endl;
+    chess.clear();
+    chess.show();
+
+    chess.add_piece('K', 'C', 3);
+    chess.add_piece('q', 'D', 5);
+    chess.add_piece('M', 'G', 2);
+    chess.add_piece('m', 'F', 6);
+    chess.show();
+
+    chess.move('C', 3, 'D', 5);
+    chess.show();
+    chess.move('F', 6, 'E', 6);
+    chess.show();
+
+
 }
