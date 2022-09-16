49d55afc28125b602fa82c63d531bf5e9e9d5a3e
Anatoly Samaris <92950839+AnatolySamaris@users.noreply.github.com>
2022-09-16 10:04:23 +0300

    Ready to work

--- Lab 1/ChessTable.cpp
+++ Lab 1/ChessTable.cpp
@@ -1,17 +1,20 @@
 #include "ChessTable.h"
+#include <iostream>
+
+using namespace std;
 
 int ChessTable::char_to_index(char letter)
 {
-    return (int)letter - 65;
+    return (int)letter - 65; // Используем код буквы в ASCII и вычитаем код буквы "А". Тогда А -> 0, B -> 1...
 }
 
 char ChessTable::fill_empty(int x, int y)
 {
-    if ((x + y) % 2 == 0) return '-';
+    if ((x + y) % 2 == 0) return '-'; // Сумма координат четная => клетка "белая", иначе - "черная"
     else return '+';
 }
 
-ChessTable::ChessTable()
+ChessTable::ChessTable() // Заполняем поле "+" и "-" по указанному в fill_empty правилу
 {
     for (int i = 0; i < 8; i++)
     {
@@ -22,22 +25,25 @@ ChessTable::ChessTable()
         }
     }
 
+    // Для удобства пользователя сразу выводим помощь и показываем поле
     cout << "New game has been created!\n";
     help();
     show();
 }
 
-void ChessTable::add_figure(char figure, char y, int x)
+// Добавляет фигуру. Параметры: буква-код фигуры, буква-координата, цифра-координата
+void ChessTable::add_piece(char piece, char y, int x)
 {
-    field[8-x][char_to_index(y)] = figure;
-    cout << "The piece " << figure << " is added to the square " << y << x << endl;
+    field[8-x][char_to_index(y)] = piece;
+    cout << "The piece " << piece << " is added to the square " << y << x << endl;
 }
 
+// Ход фигурой со "старых" координат на "новые". Если на "новой" клетке противник - бьет его.
 void ChessTable::move(char y_old, int x_old, char y_new, int x_new)
 {
-    if(field[8-x_old][char_to_index(y_old)] != '-' && field[8-x_old][char_to_index(y_old)] != '+')
+    if(field[8-x_old][char_to_index(y_old)] != '-' && field[8-x_old][char_to_index(y_old)] != '+') // Если "старая" клетка занята...
     {
-        if(field[8-x_new][char_to_index(y_new)] != '-' && field[8-x_new][char_to_index(y_new)] != '+')
+        if(field[8-x_new][char_to_index(y_new)] != '-' && field[8-x_new][char_to_index(y_new)] != '+') // Если "новая" клетка занята...
         {
             char killed = field[8-x_new][char_to_index(y_new)];
             field[8-x_new][char_to_index(y_new)] = field[8-x_old][char_to_index(y_old)];
@@ -59,31 +65,30 @@ void ChessTable::help()
     cout << "\n--- HELP ---\n";
     cout << "Pieces and their signs: P/p - pawn, R/r - rook, K/k - knight, B/b - bishop, Q/q - queen, M/m - king\n";
     cout << "Pieces of two players are divided by uppercase and lowercase signs\n\n";
-    cout << "AUTO - to fill the board automatically\n";
-    cout << "CLEAR - to clear the board\n";
-    cout << "ADD <SIGN> <CHAR_INDEX> <INT_INDEX> - to add a piece\n";
-    cout << "MOVE <CHAR_INDEX_NOW> <INT_INDEX_NOW> <CHAR_INDEX_NEW> <INT_INDEX_NEW> - to make a move or to attack\n";
-    cout << "SHOW - to show the board\n\n";
-    cout << "HELP to show help\n";
-    cout << "END to stop the game\n\n";
+    cout << ".autofill() - to fill the board automatically\n";
+    cout << ".clear() - to clear the board\n";
+    cout << ".add_piece(SIGN, CHAR_INDEX, INT_INDEX) - to add a piece\n";
+    cout << ".move(CHAR_INDEX_NOW, INT_INDEX_NOW, CHAR_INDEX_NEW, INT_INDEX_NEW) - to make a move or to attack\n";
+    cout << ".show() - to show the board\n\n";
+    cout << ".help() - to show help\n";
 }
 
-void ChessTable::show()
+void ChessTable::show() // Выводит игровое поле на данный момент
 {
-    cout << "\n  A B C D E F G H\n";
+    cout << "\n | A B C D E F G H |\n---------------------\n";
     for (int i = 0; i < 8; i++)
     {
-        cout << 8-i << ' ';
+        cout << 8-i << "| ";
         for (int j = 0; j < 8; j++)
         {
             cout << field[i][j] << ' ';
         }
-        cout << '\n';
+        cout << '|' << 8-i << '\n';
     }
-    cout << '\n';
+    cout << "---------------------\n | A B C D E F G H |\n\n";
 }
 
-void ChessTable::autofill()
+void ChessTable::autofill() // Автозаполнение поля для игры в шахматы
 {
     for (int i = 0; i < 8; i++)
     {
@@ -114,7 +119,7 @@ void ChessTable::autofill()
     cout << "Autofill completed!\n";
 }
 
-void ChessTable::clear()
+void ChessTable::clear() // Очистить игровое поле от всех фигур
 {
     for (int i = 0; i < 8; i++)
     {
