24cb956dcf922ea440511b552a370d7afbabf077
Anatoly Samaris <92950839+AnatolySamaris@users.noreply.github.com>
2022-09-30 22:14:12 +0300

    Ready to work

--- Lab3/main.cpp
+++ Lab3/main.cpp
@@ -3,124 +3,90 @@
 
 using namespace std;
 
+// Базовый абстрактный класс, являющийся интерфейсом для производных классов,
+// определяющих различные вектор-функции (ниже - MyFunction).
 class VectorFunction
 {
+    // Интерфейс рассчитан на вектор-функции размера 2,
+    // поэтому в protected принимаются два набора коэффициентов,
+    // причем первая функция имеет 3 к-та, вторая - 2.
 protected:
-    const double null = -999999;
+    double coefs_one[3];
+    double coefs_two[2];
     double answer[2];
-    double coefs[3];
-    double argument = null;
 
 public:
-    virtual void calculate(double x) = 0;
-    virtual void set_coefs(double a, double b, double c) = 0;
-    virtual void show_coefs() = 0;
+    virtual void set_coefs_one(double *coefs) = 0; // Задать к-ты для первой функции
+    virtual void set_coefs_two(double *coefs) = 0; // Задать к-ты для второй функции
+    virtual void calculate(double x) = 0; // Посчитать значение вектор-функции от аргумента х
 
-    double get_ans(int i)
+    double get_ans(int i) // вывести i-тый (начиная с 1) элемент вектор-функции
     {
-        return answer[i-1];
-    }
-
-    void show_result()
-    {
-        cout << "f1(" << argument << ") = " << answer[0] << endl;
-        cout << "f2(" << argument << ") = " << answer[1] << endl;
+        return this->answer[i-1];
     }
 };
 
 
-class FunctionOne: public VectorFunction
+class MyFunction: public VectorFunction // Наследование методов и свойств из класса VectorFunction
 {
 public:
+    void set_coefs_one(double *coefs) override // Задать к-ты для первой функции. Передается массив
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            this->coefs_one[i] = coefs[i];
+        }
+    }
+
+    void set_coefs_two(double *coefs) override // Задать к-ты для второй функции. Передается массив
+    {
+        for (int i = 0; i < 2; i++)
+        {
+            this->coefs_two[i] = coefs[i];
+        }
+    }
+
+    // Результаты вычислений записываются в результирующий вектор,
+    // определенный в интерфейсе через массив длины 2.
+    // При помощи унаследованного метода get_ans(int i) можно затем
+    // обратиться к любому из элементов результирующего вектора.
     void calculate(double x) override
     {
-        if (argument == null)
-        {
-            argument = x;
-            answer[0] = this->coefs[0] * cos(this->coefs[1] * x) + this->coefs[2];
-        }
-        else if (x == argument)
-        {
-            answer[0] = this->coefs[0] * cos(this->coefs[1] * x) + this->coefs[2];
-        }
-        else
-        {
-            cout << "Argument x must be equal in every function!\nCalculating with x in the first function.\n";
-            answer[0] = this->coefs[0] * cos(this->coefs[1] * argument) + this->coefs[2];
-        }
-    }
-
-    void set_coefs(double a, double b, double c) override
-    {
-        this->coefs[0] = a;
-        this->coefs[1] = b;
-        this->coefs[2] = c;
-    }
-
-    void show_coefs() override
-    {
-        cout << this->coefs[0] << ' ' << this->coefs[1] << ' ' << this->coefs[2] << endl;
-    }
-};
-
-
-class FunctionTwo: public VectorFunction
-{
-public:
-    void calculate(double x) override
-    {
-        if (argument == null)
-        {
-            argument = x;
-            answer[1] = this->coefs[0] * pow(x, this->coefs[1]);
-        }
-        else if (x == argument)
-        {
-            answer[1] = this->coefs[0] * pow(x, this->coefs[1]);
-        }
-        else
-        {
-            cout << "Argument x must be equal in every function!\nCalculating with x in the first function.\n";
-            answer[1] = this->coefs[0] * pow(x, this->coefs[1]);
-        }
-    }
-
-    void set_coefs(double a, double b, double c=0) override
-    {
-        this->coefs[0] = a;
-        this->coefs[1] = b;
-    }
-
-    void show_coefs() final
-    {
-        cout << this->coefs[0] << ' ' << this->coefs[1] << endl;
+        this->answer[0] = this->coefs_one[0] * cos(x * this->coefs_one[1]) + this->coefs_one[2];
+        this->answer[1] = this->coefs_two[0] * pow(x, this->coefs_two[1]);
     }
 };
 
 int main()
 {
-    FunctionOne f1 = FunctionOne();
-    FunctionTwo f2 = FunctionTwo();
+    MyFunction vf = MyFunction(); // Создаем объект класса MyFunction. Создание вектор-функции
 
-    f1.set_coefs(1, 2, 3);
-    f2.set_coefs(10, 2);
+    // Определяем коэффициенты первой и второй функции созданной вектор-функции
+    double coefs1[3], coefs2[2];
+    cout << "Enter coefficients a, b, c for the first function:\n";
+    cin >> coefs1[0] >> coefs1[1] >> coefs1[2];
+    vf.set_coefs_one(coefs1);
 
-    f1.show_coefs();
-    f2.show_coefs();
+    cout << "Enter coefficients a, b for the second function:\n";
+    cin >> coefs2[0] >> coefs2[1];
+    vf.set_coefs_two(coefs2);
 
-    f1.calculate(5);
-    f2.calculate(5);
+    // Получаем массив аргументов
+    int x_size;
+    cout << "Enter amount of x values:\n";
+    cin >> x_size;
+    double x_values[x_size];
+    cout << "Enter x-values. If their amount is more than 1, separate them with space:\n";
+    for (int i = 0; i < x_size; i++)
+    {
+        cin >> x_values[i];
+    }
 
-    cout << f1.get_ans(1) << ' ' << f1.get_ans(2) << endl;
-
-    f1.show_result();
-
-    cout << endl;
-    f1.set_coefs(3, 3, 3);
-    f2.set_coefs(1, 5);
-
-    f1.calculate(10);
-    f2.calculate(5);
-    f1.show_result();
+    // Вычисляем результат для каждого аргумента и выводим в консоль поэлементно
+    for (int i = 0; i < x_size; i++)
+    {
+        vf.calculate(x_values[i]);
+        cout << "For x = " << x_values[i] << " vector-function is (" << vf.get_ans(1) << ' ' << vf.get_ans(2) << ");\n";
+    }
     return 0;
 }
